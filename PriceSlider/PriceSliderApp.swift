1a7fcf5081b55e256b5836d6448a681aa8ec2f59
Israel Torres Alvarado <israel.torres@elektra.com.mx>
2025-05-13 08:29:08 -0600

    Initial Commit

---
+++ PriceSlider/PriceSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PriceSliderApp.swift
+//  PriceSlider
+//
+//  Created by Israel Torres Alvarado on 13/05/25.
+//
+
+import SwiftUI
+
+@main
+struct PriceSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PriceSliderTests/PriceSliderTests.swift
@@ -0,0 +1,17 @@
+//
+//  PriceSliderTests.swift
+//  PriceSliderTests
+//
+//  Created by Israel Torres Alvarado on 13/05/25.
+//
+
+import Testing
+@testable import PriceSlider
+
+struct PriceSliderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
